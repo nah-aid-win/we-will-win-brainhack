275d0352da4f7eeff07f966435e45ba25496b814
timetraveller-123 <111265952+timetraveller-123@users.noreply.github.com>
2024-05-26 21:21:54 +0800

    Update VLMManager.py

--- vlm/src/VLMManager.py
+++ vlm/src/VLMManager.py
@@ -1,11 +1,32 @@
 from typing import List
-
+from transformers import AutoProcessor, AutoModelForZeroShotObjectDetection
+import io
+from PIL import Image
+import torch
 
 class VLMManager:
     def __init__(self):
-        # initialize the model here
-        pass
-
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.model = AutoModelForZeroShotObjectDetection.from_pretrained("google/owlv2-base-patch16").to(self.device)
+        self.processor = AutoProcessor.from_pretrained("google/owlv2-base-patch16")
+        self.model.eval()
     def identify(self, image: bytes, caption: str) -> List[int]:
-        # perform object detection with a vision-language model
-        return [0, 0, 0, 0]
+        im = Image.open(io.BytesIO(image))
+        inputs = self.processor(text=[caption], images=im, return_tensors="pt").to(self.device)
+        
+        with torch.no_grad():
+            outputs = self.model(**inputs)
+            target_sizes = torch.tensor([im.size[::-1]]).to(self.device)
+            results = self.processor.post_process_object_detection(outputs, threshold=0.1, target_sizes=target_sizes)[0]
+        
+        
+        ratio = 1520/870
+        boxes = results["boxes"].tolist()
+        if(len(boxes) == 0):
+            return [0,0,1520,870]
+        scores = results["scores"].tolist()
+        m = max(scores)
+        xmin, ymin, xmax, ymax = boxes[scores.index(m)]
+        ymin*=ratio
+        ymax*=ratio
+        return [int(xmin),int(ymin),int(xmax-xmin),int(ymax-ymin)]
